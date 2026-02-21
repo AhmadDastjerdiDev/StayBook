8b4b7288602c19d8bd11b35c5ce29c3b0d9efee2
Ahmad <ahmaddastjerdi.eng@gmail.com>
2026-02-21 20:13:13 +0000

    Add paginated accommodation listing endpoint

---
+++ src/main/java/sia/staybook/web/controller/AccommodationController.java
@@ -0,0 +1,27 @@
+package sia.staybook.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sia.staybook.dto.AccommodationResponseDto;
+import sia.staybook.service.AccommodationService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/accommodations")
+public class AccommodationController {
+
+    private final AccommodationService accommodationService;
+
+    @GetMapping
+    public Page<AccommodationResponseDto> getAllAccommodations(
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC)
+            Pageable pageable){
+       return accommodationService.getAllAccommodations(pageable);
+    }
+}
